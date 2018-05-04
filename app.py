c9ae3322e48c6325db31cb5c143797f1ed87b362
Marcelo <marcelonunes3698@gmail.com>
2018-05-04 09:37:35 -0300

    add parte 1 - areas + csv import

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,request, make_response
+# encoding: utf-8
+from flask import Flask, abort, make_response
 import flask
 import re
 import csv
@@ -6,34 +7,59 @@ import StringIO
 
 app = Flask(__name__)
 
+CSV_DIR = 'CSIndex\\data\\'
 
-#MÉTODO PADRÃO - ATIVA O BOTÃO PARA TESTES
+def ler_arquivo_para_list(nome_arquivo):
+    with open(nome_arquivo, 'rU') as ficheiro:
+        reader = csv.reader(ficheiro, delimiter=',', quoting=csv.QUOTE_NONE)
+        lista = []
+        for read in reader:
+            lista.append(read)
+        return lista
+
+
 @app.route('/')
 def landing():
-    return """<form action="data" method="post">
-            <input type="submit" name="submit" value="Get Data">
-         </form>"""
+    return "Para requisitar número de conferencias em uma area, acesse: =>  " \
+           "<a href=\"buscar_conferencias_por_area/nomedaarea/NOMEDACONFERENCIA\"> " \
+           "127.0.0.1:5000/buscar_conferencias_por_area/nomedaarea/NOMEDACONFERENCIA" \
+           " </a>"
 
 
-# MÉTODO PARA CRIAÇÃO DA PLANILHA CSV
-@app.route('/data', methods=['POST'])
-def data():
-    if request.method == 'POST':
-        if request.form['submit'] == 'Get Data':
-            csvlist = [['item 1', 'item2'], ['box1', 'box2']]
+@app.route('/buscar_conferencias_por_area/<string:area>/<string:conferencia>', methods=['GET', 'POST'])
+def data(area, conferencia):
+    ok_area = 0
+    quantidade_conferencias = 0
 
-            si = StringIO.StringIO()
-            cw = csv.writer(si)
-            cw.writerows(csvlist)
-            output = make_response(si.getvalue())
-            output.headers["Content-Disposition"] = "attachment; filename=export.csv"
-            output.headers["Content-type"] = "text/csv"
-            return output
+    # Verificacao da integridade do nome da area passada, ele deve estar na planilha areas.csv da pasta do arquivo
+    areas = ler_arquivo_para_list("areas.csv")
+    for item in areas:
+        #Se o nome da area vinda estiver na planilha de areas, podemos prosseguir.
+        if str(item)[2:-2] == area:
+            ok_area = 1
 
+    # CONFERE SE A AREA PASSADA ESTÁ ENTRE OS NOMES EXISTENTES DE ÁREAS, SENÃO, GERA O ERRO 404.
+    if ok_area == 1:
+        caminho = CSV_DIR+area+"-out-confs.csv"
+        arquivos = ler_arquivo_para_list(caminho)
 
+        for arquivo in arquivos:
+            if arquivo[0] == conferencia:
+                quantidade_conferencias += int(arquivo[1])
 
-#MÉTODO PARA BAIXAR A PLANILHA DO DIRETÓRIO
-CSV_DIR = 'CSIndex\\data\\'
+        saida = []
+        saida.append(quantidade_conferencias)
+        si = StringIO.StringIO()
+        cw = csv.writer(si)
+        cw.writerow(saida)
+        output = make_response(si.getvalue())
+        output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        output.headers["Content-type"] = "text/csv"
+
+        return output
+        # return str(quantidade_conferencias)
+    else:
+        abort(404)
 
 # Allow the filename to be included in the path
 @app.route('/risky_csv_sender/<filename>')
